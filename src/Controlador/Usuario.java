488ab9bee4ef402bc1d9f273b76e09f8ebf33871
Ariel Leandro Vera Vargas <52182623+Arieleandrovv@users.noreply.github.com>
2023-04-04 12:47:56 -0400

    "nuevos paquetes creados"

---
+++ src/Controlador/Usuario.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import javax.swing.JTable;
+
+/**
+ *
+ * @author PC
+ */
+public class Usuario {
+    public String nombreDeUsuario;
+    public String carnetDeIdentidad;
+    public String telefono;
+    public long fechaDeInicio;
+    public String idUsuario;
+    public String Contrasenia;
+    public String direccion;
+    public JTable tabla;
+
+    public Usuario() {
+        
+    }
+    
+}
