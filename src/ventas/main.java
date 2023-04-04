856aa5b5c7e6bf3f7da52027ad35c0cdb9062aed
Arieleandrovv <Arieleandrovv@outlook.com>
2023-04-04 00:28:56 -0400

    clase main

---
+++ src/ventas/main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ventas;
+
+import Modelo.ConectarBD;
+import Vista.InterfazAdministrarUsuario;
+
+
+/**
+ *
+ * @author HP
+ */
+public class main {
+    public static void main (String[] args){
+        InterfazAdministrarUsuario ventana=new InterfazAdministrarUsuario();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+}
+}
