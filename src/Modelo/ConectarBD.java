c57364d6ae84317de249258a56120007713db2ae
Andrea Heredia <andreaheredia1339@gmail.com>
2023-04-08 15:45:49 -0400

    cambios idk

--- src/Modelo/ConectarBD.java
+++ src/Modelo/ConectarBD.java
@@ -17,10 +17,10 @@ public class ConectarBD {
     Connection conectar = null;
     
     String user = "root";
-    String password = "root";//*********** colocar el password de su base de datos
+    String password = "asdfg";//*********** colocar el password de su base de datos
     String dataBase = "ventasgobd";
     String server = "localhost";
-    String puerto = "3308";
+    String puerto = "3306";
     String driver = "com.mysql.cj.jdbc.Driver";
     
     String cadena = "jdbc:mysql://"+server+":"+puerto+"/"+dataBase;
