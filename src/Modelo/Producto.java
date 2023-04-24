719625202547ab386ca019de4134d06759dbc955
Franz Carrillo <franzluiz10@gmail.com>
2023-04-23 20:11:43 -0400

    Interfaz de lista de productos

---
+++ src/Modelo/Producto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author HP PAVILION
+ */
+public class Producto {
+    public String idProducto;
+    public String Marca;
+    public String Modelo;
+    public String Precio;
+    public String Caracteristicas;
+    public String Fotografia;
+}
